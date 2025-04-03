84f8ff39a7dfbf9287bb293c6dc2759a305558b0
Ruslan Romanenko <71728377+r-romanenko@users.noreply.github.com>
2025-04-03 14:45:42 -0700

    Working how I want it to!

--- shiny_hunter.py
+++ shiny_hunter.py
@@ -16,48 +16,39 @@ def current_milli_time():
     return round(time.time() * 1000)
 
 
-
 sleep_time = 3 # seconds to wait to allow user to switch to emulator
 # seconds_to_wait = 3 # seconds to wait for user to be still (irrelevant)
 confirm_key = 'x' # the key used to proceed with the program's steps
+begin_hunt_key = 'h'
 seconds_between_check = 5 # default value to mash x for before checking the pixel color
+
 
 print(f"Sleeping for {sleep_time} seconds. Use this time to switch to Pokemon window.")
 time.sleep(sleep_time)
 
 
-start_time = current_milli_time()
-
-print(f"Hover over the Reset button and press \'{confirm_key}\' ")
-
-reset_x, reset_y = pyautogui.position()
-
-
-keyboard.wait('x')
-print(f"reset x: {reset_x}, reset y: {reset_y}")
-reset_x, reset_y = pyautogui.position()
-
-
-# then start counting for 5 seconds
-start_time = current_milli_time()
-
-
 print(f"Hover over Tepig and press \'{confirm_key}\'")
 keyboard.wait('x')
 x, y = pyautogui.position()
-
-# one more thing I want to do is to have the user define how long to wait
-
-
-
-
-print(f"Coordinates found! x: {x}, y: {y}")
-
 # now we have the x and y of the pixel (or at least close) of what we want to look at
 image = ImageGrab.grab()
 color = image.getpixel((x, y))
 print(f"color at {x}, {y} is: {color}")
 
+
+print(f"\nHover over the Reset button and start mashing \'{confirm_key}\'.")
+print(f"Press \'{begin_hunt_key}\' once Tepig is in battle and has been on the ground for at least about a second.")
+keyboard.wait('x')
+reset_x, reset_y = pyautogui.position()
+print(f"\nreset x: {reset_x}, reset y: {reset_y}")
+pyautogui.click()
+
+seconds_between_check_timer = current_milli_time()
+keyboard.wait('h')
+seconds_between_check = round((current_milli_time() - seconds_between_check_timer) / 1000)
+print(f"Seconds between check set to {seconds_between_check}.\n\n")
+
+print(f"The hunt has begun! Feel free to get up and leave. Eventually, there will be a shiny Tepig on your screen.")
 
 # now I know where to check the color
 # I need to now wait again for a certain amount of time, while mashing x
